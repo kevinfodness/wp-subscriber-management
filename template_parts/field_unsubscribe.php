e70b261756242395c688b549bb3306e2ca610f30
Kevin Fodness <kevin@kevinfodness.com>
2020-08-29 08:45:38 -0400

    Use WP table layout for usermeta fields

--- template_parts/field_unsubscribe.php
+++ template_parts/field_unsubscribe.php
@@ -10,19 +10,28 @@
 
 ?>
 
-<div>
-    <label for="wp-subscriber-management-unsubscribe">
-        <input
-            <?php checked( true, $unsubscribed ); ?>
-            id="wp-subscriber-management-unsubscribe"
-            name="wp-subscriber-management-unsubscribe"
-            type="checkbox"
-        />
-        <?php
-            esc_html_e(
-                'Unsubscribe from Email Notifications',
-                'wp-subscriber-management'
-            );
-        ?>
-    </label>
-</div>
+<h2><?php esc_html_e( 'Unsubscribe', 'wp_subscriber_management' ); ?></h2>
+<table class="form-table" role="presentation">
+    <tbody>
+        <tr id="wp-subscriber-management" class="wp-subscriber-management-wrap">
+            <th>
+                <label for="wp-subscriber-management-unsubscribe">
+                    <?php
+                        esc_html_e(
+                            'Unsubscribe from Email Notifications',
+                            'wp-subscriber-management'
+                        );
+                    ?>
+                </label>
+            </th>
+            <td>
+                <input
+                    <?php checked( true, $unsubscribed ); ?>
+                        id="wp-subscriber-management-unsubscribe"
+                        name="wp-subscriber-management-unsubscribe"
+                        type="checkbox"
+                />
+            </td>
+        </tr>
+    </tbody>
+</table>
