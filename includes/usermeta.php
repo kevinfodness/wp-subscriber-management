da0dcd105bf538fe45a1c7d1d23f6b017a62f296
Kevin Fodness <kevin@kevinfodness.com>
2020-08-30 11:05:56 -0400

    Add unsubscribe via flag

--- includes/usermeta.php
+++ includes/usermeta.php
@@ -50,6 +50,52 @@ class Usermeta {
     }
 
     /**
+     * Initializes functionality by setting up action and filter hooks.
+     */
+    public static function init()
+    {
+        add_action(
+            'edit_user_profile',
+            [self::class, 'field_unsubscribe']
+        );
+        add_action(
+            'edit_user_profile_update',
+            [self::class, 'save_unsubscribe']
+        );
+        add_action(
+            'personal_options_update',
+            [self::class, 'save_unsubscribe']
+        );
+        add_action(
+            'show_user_profile',
+            [self::class, 'field_unsubscribe']
+        );
+        add_action(
+            'template_redirect',
+            [self::class, 'process_unsubscribe']
+        );
+    }
+
+    /**
+     * Processes an unsubscribe if the ?unsubscribe flag is set.
+     */
+    public static function process_unsubscribe() {
+        // Ensure the unsubscribe flag is set.
+        if ( empty( $_GET['unsubscribe'] ) ) {
+            return;
+        }
+
+        // Try to get a user by the given email address.
+        $user = get_user_by( 'email', sanitize_email( $_GET['unsubscribe'] ) );
+        if ( false === $user ) {
+            return;
+        }
+
+        // Unsubscribe the user.
+        self::set_unsubscribe( $user->ID, true );
+    }
+
+    /**
      * Saves the unsubscribe field.
      *
      * @param int $user_id The ID of the user profile being edited.
@@ -57,6 +103,11 @@ class Usermeta {
      * @return bool True if save was successful, false otherwise.
      */
     public static function save_unsubscribe( int $user_id ) : bool {
+        // Permissions check.
+        if ( ! current_user_can( 'edit_user', $user_id ) ) {
+            return false;
+        }
+
         return self::set_unsubscribe(
             $user_id,
             isset( $_POST['wp-subscriber-management-unsubscribe'] )
@@ -72,11 +123,6 @@ class Usermeta {
      * @return bool True if successful, false if not.
      */
     public static function set_unsubscribe( int $user_id, bool $value ) : bool {
-        // Permissions check.
-        if ( ! current_user_can( 'edit_user', $user_id ) ) {
-            return false;
-        }
-
         // If the current value is the same as the new value, don't do anything.
         if ( $value === self::get_unsubscribe( $user_id ) ) {
             return true;
@@ -95,27 +141,5 @@ class Usermeta {
                 self::UNSUBSCRIBE_FIELD
             );
         }
-    }
-
-    /**
-     * Initializes functionality by setting up action and filter hooks.
-     */
-    public static function init() {
-        add_action(
-            'edit_user_profile',
-            [ self::class, 'field_unsubscribe' ]
-        );
-        add_action(
-            'edit_user_profile_update',
-            [ self::class, 'save_unsubscribe' ]
-        );
-        add_action(
-            'personal_options_update',
-            [ self::class, 'save_unsubscribe' ]
-        );
-        add_action(
-            'show_user_profile',
-            [ self::class, 'field_unsubscribe' ]
-        );
     }
 }
